ebbe467913e7cdb91b3543f3052d8d7da853f490
Marinel <102971709+Marinel-V@users.noreply.github.com>
2024-02-08 23:57:14 -0500

    added sveltekit + firebase project demo

---
+++ my-app/.firebase/eccip-18b4b/functions/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.iBibWAFB.js","_app/immutable/chunks/scheduler.O8VUTp4Y.js","_app/immutable/chunks/index.j2K9MSZv.js","_app/immutable/chunks/stores.ePx23Llx.js","_app/immutable/chunks/entry.sLl5J7Kc.js","_app/immutable/chunks/index.Czk_y0aN.js"];
+export const stylesheets = [];
+export const fonts = [];
---
+++ my-app/.firebase/eccip-18b4b/hosting/_app/immutable/entry/start.Y7jYRfEx.js
@@ -0,0 +1,1 @@
+import{b as a}from"../chunks/entry.sLl5J7Kc.js";export{a as start};
